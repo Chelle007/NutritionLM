c420d6c0e1e60c68365e3e0f90357a067b31c195
desraymondz <desraymondz@gmail.com>
2025-11-29 22:22:55 +0800

    Enhanced google login button

--- app/login/page.js
+++ app/login/page.js
@@ -2,6 +2,7 @@
 "use client";
 
 import { createClient } from '../utils/supabase/client';
+import Image from 'next/image';
 
 export default function Login() {
 
@@ -25,9 +26,16 @@ export default function Login() {
     };
 
     return (
-        <div>
-            <button onClick={handleSignInWithGoogle}>
-                Login with Google
+        <div className="flex flex-col justify-center items-center h-screen">
+            <button 
+                className="group h-12 px-6 border-2 border-gray-300 rounded-full" 
+                onClick={handleSignInWithGoogle}>
+                <div className="flex items-center space-x-4 justify-center">
+                    <Image src="https://www.svgrepo.com/show/475656/google-color.svg" alt="google logo" width={20} height={20} />
+                    <span className="font-semibold tracking-wide text-gray-700 dark:text-white text-sm">
+                        Continue with Google
+                    </span>
+                </div>
             </button>
         </div>
     );
