200a6cca0bffbcff71d5c9c9e8ca8284a7609965
desraymondz <desraymondz@gmail.com>
2025-12-01 15:17:43 +0800

    Update auth.js

--- services/auth.js
+++ services/auth.js
@@ -1,8 +1,9 @@
-import supabase from './supabaseClient'
+import getSupabaseClient from './supabaseClient'
 
 // Used to check if the user is authenticated or not
 // Call this function before accessing any user data
 export default async function getAuthenticatedUser() {
+  const supabase = getSupabaseClient();
   const { data: { user }, error } = await supabase.auth.getUser();
   if (error || !user) throw new Error('User not authenticated');
 
